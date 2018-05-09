eabaeae4839347dd1f86006cb829ac42f8641db9
tungjd <tungjd@gmail.com>
2018-05-09 14:25:55 -0700

    Adding Hyperfish Socket Test Script

---
+++ Utilities/HFSocketTest.ps1
@@ -0,0 +1,48 @@
+﻿function testAPP
+{
+$appep = @("a1.hyperfish.com","a2.hyperfish.com")
+$appport = "443"
+
+    foreach ($endpoint in $appep)
+    {
+        try {
+            write-host "testing" $endpoint "over" $appport 
+            (New-Object System.Net.Sockets.TcpClient).Connect($endpoint,$appport)
+            write-host $endpoint -NoNewline -ForegroundColor Yellow 
+            write-host " is " -NoNewline
+            write-host "GOOD" -ForegroundColor Green
+            }
+
+        catch
+            {
+            write-host "Unable to connect to" $endpoint -ForegroundColor Red
+            }
+    }
+}  
+
+function testAMQP
+{
+$mqep = @("q01.hyperfish.com","q02.hyperfish.com")
+$mqport = "5671"
+
+    foreach ($endpoint in $mqep)
+    {
+        try {
+            write-host "testing" $endpoint "over" $mqport 
+            (New-Object System.Net.Sockets.TcpClient).Connect($endpoint,$mqport)
+            write-host $endpoint -NoNewline -ForegroundColor Yellow 
+            write-host " is " -NoNewline
+            write-host "GOOD" -ForegroundColor Green
+            }
+
+        catch
+            {
+            write-host "Unable to resolve" $endpoint -ForegroundColor Red
+            }
+    }
+}
+
+Write-host "Testing APP Connectivity"
+testAPP
+Write-host "Testing AMQPS Connectivity"
+testAMQP
